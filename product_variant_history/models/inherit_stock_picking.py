234fbcfb104db19e5f410e762ae5ae91131e6d40
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2018-02-28 10:15:11 +0600

    [IMP]Product List update from stock move

--- product_variant_history/models/inherit_stock_picking.py
+++ product_variant_history/models/inherit_stock_picking.py
@@ -23,13 +23,23 @@ class InheritStockPicking(models.Model):
 
                 avg_price = self.env['product.variant.history'].search([('product_id', '=', record.product_id.id)],
                                                                        order='effective_datetime DESC', limit=1)
+                avg = (sum * avg_price.value + record.ordered_qty * record.price_unit) / (sum + record.ordered_qty)
 
                 if 'date_done' in record.picking_id:
                     date_done = record.picking_id.date_done
+                    # done_datetime = datetime.strptime(date_done, "%Y-%m-%d %H:%M:%S")
+                    product_list = []
+                    for move_line in self.move_lines:
+                        product_list.append(move_line.product_id.id)
+                    date_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    if date_done < date_now:
+                        stock_move_objs = self.env['stock.move'].search([('date','>=',date_done),('product_id','=',record.product_id.id)])
+                        for stock_move_obj in stock_move_objs:
+                            stock_move_obj.write({})
                 else:
                     date_done = datetime.now()
 
-                avg = (sum * avg_price.value + record.ordered_qty * record.price_unit) / (sum + record.ordered_qty)
+
                 self.env['product.variant.history'].create({
                     'product_id': record.product_id.id,
                     'product_tmpl_id': record.product_id.product_tmpl_id.id,
